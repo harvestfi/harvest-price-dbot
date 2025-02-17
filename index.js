c0f42b95c5cf28848d12ed7adf91cd76ed948714
william <williamliu9426@gmail.com>
2025-02-17 03:55:06 -0500

    fix: add zksync's chain name

--- index.js
+++ index.js
@@ -53,6 +53,9 @@ const getChainName = chain => {
     case CHAIN_IDS.BASE:
       chainName = 'Base'
       break
+    case CHAIN_IDS.ZKSYNC:
+      chainName = 'Zksync'
+      break
     default:
       chainName = 'Ethereum'
       break
